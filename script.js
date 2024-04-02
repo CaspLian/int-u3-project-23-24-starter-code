595e40edbab5f79b3bcf1e7159d9e0025b31562a
CaspLian <149088248+CaspLian@users.noreply.github.com>
2024-04-02 15:25:29 +0000

    4/2

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
+let beginScreen = document.querySelector(".story-opening")
+let optionOneScreen = document.querySelector(".option-one-screen")
+let optionTwoScreen = document.querySelector(".option-two-screen")
+let optionOneThirdScreen1 = document.querySelector(".option-one-screen-c1")
+let optionOneThirdScreen2 = document.querySelector(".option-one-screen-c2")
+let optionTwoThirdScreen1 = document.querySelector(".option-two-screen-c1")
+let optionTwoThirdScreen2 = document.querySelector(".option-two-screen-c2")
+let optionOneEnd1_1 = document.querySelector(".option-one-end-c1")
+let optionOneEnd1_2 = document.querySelector(".option-one-end-c2")
+let optionTwoEnd2_1 = document.querySelector(".option-two-end-c1")
+let optionTwoEnd2_2 = document.querySelector(".option-two-end-c2")
+let button1 = document.querySelector(".option-one")
+let button2 = document.querySelector(".option-two")
+let button1_1 = document.querySelector(".option-one-button1")
+let button1_2 = document.querySelector(".option-one-button2")
+let button2_1 = document.querySelector(".option-two-button1")
+let button2_2 = document.querySelector(".option-two-button2")
 
 
 
